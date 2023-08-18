a32bb8e590cfb93551cfec59eb2335c1da543ca1
MTariq99 <tariqwc99@gmail.com>
2023-08-18 15:39:21 +0500

    jimdo webiste clone using html css

---
+++ js/index.js
@@ -0,0 +1,5 @@
+// console.log('welcome to ja');
+// document.getElementsByClassName("dropdown").addEventListener("mouseenter", ()=>{
+//     document.getElementsByClassName("dropdown").style.backgroundColor = "white";
+//     }
+//     );
